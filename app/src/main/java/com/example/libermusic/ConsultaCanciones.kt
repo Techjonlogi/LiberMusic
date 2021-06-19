992438a7f818d0329eebce7902303730d2e26506
jonathan <jhoni65475@gmail.com>
2021-06-18 19:25:15 -0500

    Interfas de las actividades a realizar

---
+++ app/src/main/java/com/example/libermusic/ConsultaCanciones.kt
@@ -0,0 +1,11 @@
+package com.example.libermusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultaCanciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_consulta_canciones)
+    }
+}
